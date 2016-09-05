43ccbb27d598517fbe31cfdce4434a0700a1dc8e
leandrw <contato@leandrw.com.br>
2016-09-04 23:33:09 -0300

    Included Backbone as js framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,49 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 
 // plugins
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
+var sass = require('gulp-sass')
+var jshint = require('gulp-jshint')
+var concat = require('gulp-concat')
+var autoprefixer = require('gulp-autoprefixer')
 
 // styles
 gulp.task('styles', function () {
   return gulp.src('public/stylesheets/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('public/stylesheets'));
-});
+    .pipe(autoprefixer({
+      browsers: ['> 5%', 'ie >= 8']
+    }))
+    .pipe(gulp.dest('public/stylesheets'))
+})
 
 // scripts
 gulp.task('lint', function () {
   return gulp.src('public/javascripts/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+    .pipe(jshint.reporter('default'))
+})
 
-gulp.task('scripts', ['lint']);
+gulp.task('scripts', ['lint'])
 
 // watch
 gulp.task('watch', function () {
-  gulp.watch('public/stylesheets/*.scss', ['styles']);
-  gulp.watch('public/javascritps/*.js', ['scripts']);
-});
+  gulp.watch('public/stylesheets/*.scss', ['styles'])
+  gulp.watch('public/javascritps/*.js', ['scripts'])
+})
 
 // build
-gulp.task('build', ['styles', 'scripts']);
+gulp.task('build', ['styles', 'scripts'])
+
+// serve
+gulp.task('serve', function () {
+  var app = require('connect')()
+  var serveStatic = require('serve-static')
+
+  app.use(serveStatic('public'))
+
+  console.log(' ➜   Open: http://localhost:7007')
+  app.listen(7007)
+})
 
 // Default
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'watch', 'serve'])
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,52 +1,44 @@
-// your code
-var participantes = {
-  "version": "0.1.1",
-  "box_name": "A Fazenda - Ranking",
-  "data": [
-    {
-      "__id": "f8c3500f39017602234a031caa64a8b4",
-      "timestamp": 1408396531382,
-      "name": "Rita Cadillac",
-      "description": "Cracrete n&ordm;1",
-      "picture": "http://im.r7.com/record/files/2C96/1617/3F6E/32AE/013F/72F9/AD72/3CE1/RitaCadillac1.jpg",
-      "positive": 51638022,
-      "negative": 18143089
-    },
-    {
-      "__id": "7b1dd3f58be97715e9e06475bb58fce5",
-      "timestamp": 1408396481826,
-      "name": "Gominho",
-      "description": "Fofoqueiro de Plantão",
-      "picture": "http://im.r7.com/record/files/2C96/1618/3F6E/369D/013F/72F7/CF15/5F4B/Gominho1.jpg",
-      "positive": "23249923",
-      "negative": "39587707"
-    },
-    {
-      "__id": "70580002438b08c63286d08b7c43fb4c",
-      "timestamp": 1408396545027,
-      "name": "Yudi Tamashiro",
-      "description": "Apresentador e ídolo teen",
-      "picture": "http://im.r7.com/record/files/2C96/1617/3F6E/32AE/013F/72FD/87BB/4436/Yudi1.jpg",
-      "positive": 59089056,
-      "negative": 14772265
-    },
-    {
-      "__id": "3404c4a70e7704009cd1915a349189f4",
-      "timestamp": 1408396555971,
-      "name": "Andressa Urach",
-      "description": "Personalidade da mídia",
-      "picture": "http://im.r7.com/record/files/2C96/1618/3F6E/369D/013F/72EF/C598/41DC/Andressa1.jpg",
-      "positive": null,
-      "age": 32
-    },
-    {
-      "__id": "c97686edbeb8df774a567e9884f4d490",
-      "timestamp": 1408396562866,
-      "name": "Bárbara Evans",
-      "description": "Modelo e filha de Monique Evans",
-      "picture": "http://im.r7.com/record/files/2C96/1618/3F6E/369D/013F/72F6/B48C/5BBD/B%C3%A1rbaraEvans1.jpg",
-      "positive": 69274684,
-      "negative": 9446548
-    }
-  ]
-};
+// Model
+var Farmer = Backbone.Model.extend({
+  defaults: {
+    name: '',
+    description: '',
+    picture: '',
+    positive: 0,
+    negative: 0,
+    positivePercent: 0,
+    negativePercent: 0
+  }
+});
+
+// Collection
+var Farmers = Backbone.Collection.extend({
+  model: Farmer,
+  url: '/fazenda.json',
+  parse: function (data) {
+    return data.data;
+  }
+});
+
+// View
+var FarmersView = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'ranking-list',
+  initialize: function () {
+    this.collection;
+    this.listenTo(this.collection, 'all', this.render);
+  },
+  newTemplate: Handlebars.compile(document.getElementById('farmers-template').text),
+  render: function () {
+    this.collection.reduce();
+    this.$el.html(this.newTemplate({ farmers: this.collection.toJSON() }));
+  }
+});
+
+var farmers = new Farmers();
+farmers.fetch();
+
+var farmersView = new FarmersView({ collection: farmers });
+farmersView.render();
+
+document.getElementById('ranking-list').appendChild(farmersView.el);
